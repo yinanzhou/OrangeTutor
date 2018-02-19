4653e8ca3ae22a201ab61d534234295b92ddb4ca
Yinan Zhou <yzhou109@syr.edu>
2018-02-19 13:23:53 -0500

    Add educational news slides on dashboard home page

--- backend/application/portal/controller/dashboard/General.php
+++ backend/application/portal/controller/dashboard/General.php
@@ -11,6 +11,18 @@ class General extends Controller {
     if (!Auth::isLogin()) {
       return Auth::redirectToLogin($this->request);
     }
+    $this->assign('education_news',[
+      [
+        'imgUrl' => '/static/slides/phone.jpeg',
+        'title' => 'Mina Jung: "No phone in class"',
+        'description' => 'Research shows coorelation between phone usage and grade.'
+      ],
+      [
+        'imgUrl' => '/static/slides/mina.jpeg',
+        'title' => 'Learning MIPS increases IQ',
+        'description' => 'Research shows benefits of challenging brain by coding MIPS'
+      ],
+    ]);
     return view();
   }
 }
