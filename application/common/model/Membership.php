a940e305e428b911642d2f8893c9f7f0d2a153a3
KyleOrnstein <36022932+KyleOrnstein@users.noreply.github.com>
2018-03-02 15:05:55 -0500

    Update Membership.php

--- application/common/model/Membership.php
+++ application/common/model/Membership.php
@@ -8,15 +8,15 @@ class Membership extends Model
 {
   protected $pk = 'user_id,group_id';
   protected $type = [
-    'membership_validfrom' => 'datetime',
-    'membership_expiration' => 'datetime',
+    'membership_validfrom' => 'datetime',//sets the membership begining time 
+    'membership_expiration' => 'datetime',//sets the membership end time
   ];
 
   public function user() {
-    return $this->belongsTo('user','user_id','user_id');
+    return $this->belongsTo('user','user_id','user_id');//connects $this to the user groups
   }
 
   public function group() {
-    return $this->belongsTo('group','group_id','group_id');
+    return $this->belongsTo('group','group_id','group_id');//connects $this to the different groups
   }
 }
