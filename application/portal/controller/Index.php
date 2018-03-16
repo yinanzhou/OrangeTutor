e0a3fcadd052ba78d5cc876c6160fa9068f6af9d
JohnMarcus09 <36247146+JohnMarcus09@users.noreply.github.com>
2018-03-15 21:19:41 -0400

    Update Index.php

--- application/portal/controller/Index.php
+++ application/portal/controller/Index.php
@@ -1,9 +1,11 @@
+
 <?php
 
 namespace app\portal\controller;
 
 use think\Controller;
 
+// This enables an extension from the controller to the Index which for the specific user, Tutor, to view the availability.
 class Index extends Controller {
   public function index() {
     return view();
